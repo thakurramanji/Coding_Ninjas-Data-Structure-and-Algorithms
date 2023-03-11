0c1a42b625381476f581ab4ce842f18478508cd6
unknown <thakurramanji@gmail.com>
2023-03-11 12:00:44 +0530

    comment added in start

--- Condition_and_Loops/start_pattern.cpp
+++ Condition_and_Loops/start_pattern.cpp
@@ -7,7 +7,7 @@ int main(){
        /*  Read input as specified in the question.
 	* Print output as specified in the question.
 	*/
-    int n;
+    int n; //this is n
     cin>>n;
     
     for(int i=0;i<n;i++) {
