2d961c2d6869c80e193d9856555fb4dcb6343073
unknown <thakurramanji@gmail.com>
2023-03-11 09:55:28 +0530

    comment removed

--- Condition_and_Loops/fahrenheit_to_celsius_table.cpp
+++ Condition_and_Loops/fahrenheit_to_celsius_table.cpp
@@ -3,13 +3,8 @@ using namespace std;
 
 
 int main(){
-
-       /*  Read input as specified in the question.
-	* Print output as specified in the question.
-	*/
     int st,en,gap;
     cin>>st>>en>>gap;
-    
     while(st<=en) {
         int cel = (st-32)*5/9 ;
         cout<<st<<" "<<cel<<endl;
