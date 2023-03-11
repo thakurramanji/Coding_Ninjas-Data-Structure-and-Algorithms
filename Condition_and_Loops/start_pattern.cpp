431cad39e8b207e93665148d2a59c36b0abbe20c
unknown <thakurramanji@gmail.com>
2023-03-11 11:58:40 +0530

    start pattern

---
+++ Condition_and_Loops/start_pattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++) {
+        int space = n-1-i;
+        int star = 2*i+1;
+        while(space--) {
+            cout<<" ";
+        }
+        while(star--) {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+  
+}
+
+
