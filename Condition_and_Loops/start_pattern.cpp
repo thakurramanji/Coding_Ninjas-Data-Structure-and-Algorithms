f7ee9323c3a9260b2a90794df6bbaf52f0659e3a
unknown <thakurramanji@gmail.com>
2023-03-11 15:41:16 +0530

    comment removed

--- Condition_and_Loops/start_pattern.cpp
+++ Condition_and_Loops/start_pattern.cpp
@@ -3,11 +3,7 @@ using namespace std;
 
 
 int main(){
-
-       /*  Read input as specified in the question.
-	* Print output as specified in the question.
-	*/
-    int n; //this is n
+    int n;
     cin>>n;
     
     for(int i=0;i<n;i++) {
