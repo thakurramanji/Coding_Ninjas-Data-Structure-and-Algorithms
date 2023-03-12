7c4ca94c8b04804bd63711962c4c55d06e327122
unknown <thakurramanji@gmail.com>
2023-03-12 23:31:21 +0530

    lsdjf

---
+++ Pattern1/interesting_alphabets.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++) {
+        int k=i+1;
+        char c = 65+n-i-1;
+        while(k--) {
+            cout<<c;
+            c++;
+        }
+        cout<<endl;
+    }
+    
+}
