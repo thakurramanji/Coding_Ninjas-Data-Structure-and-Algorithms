b9d02708786830d1da95196545aab28d238699fe
unknown <thakurramanji@gmail.com>
2023-04-21 00:57:47 +0530

    2 code added

---
+++ test-1/Pyramid_Number_Pattern.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  // Write your code here  
+    int n;
+    cin>>n;
+    
+    for(int i=1;i<=n;i++) {
+        int space = n-i;
+        while(space--) {
+            cout<<" ";
+        }
+        int d = i;
+        int likho = i;
+        while(d--) {
+            cout<<likho;
+            likho--;
+        }
+        
+        int rev = i-1;
+        int printt= 2;
+        while(rev--) {
+            cout<<printt;
+            printt++;
+        }
+        cout<<endl;
+    }
+
+  
+}
+
---
+++ test-1/number_star_patter.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+  // Write your code here
+    int n;
+    cin>>n;
+    
+    int p=n;
+
+    int star = 0;
+    
+    while(n > 0) {
+        
+        int l=1;
+
+        while(l<=n) {
+            cout<<l;
+            l++;
+        }
+
+       
+        int st = star;
+
+        while(st>0) {
+            cout<<"*";
+            st--;
+        }
+
+        int tt = n;
+        while(tt>0) {
+            cout<<tt;
+            tt--;
+        }
+        cout<<endl;
+        star= star + 2;
+        n--;
+    }
+    
+
+}
+
+
