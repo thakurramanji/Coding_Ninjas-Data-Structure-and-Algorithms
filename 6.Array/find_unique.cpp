ea9fc727dbf80bd4459d63225cf3d65b32dcb3a8
unknown <thakurramanji@gmail.com>
2023-04-28 21:56:32 +0530

    comment removed

--- 6.Array/find_unique.cpp
+++ 6.Array/find_unique.cpp
@@ -1,27 +1,12 @@
 int findUnique(int *arr, int size)
 {
-    //Write your code here
-//     int res=0;
-//     for(int i=0;i<size;i++) {
-//         res = res^ arr[i];
-//     }
-    
-//     return res;
-    
-    
     int p=0;
-    
     for(int i=0;i<size;i++) {
-        
         p=1;
-        
-       //  2 3 1 6 3 6 2
-        
         int r = arr[i];
         for(int j=0;j<size;j++) {
             if(j==i)
                 continue;
-            
             if(arr[i]==arr[j])
                 p=2;
         }
