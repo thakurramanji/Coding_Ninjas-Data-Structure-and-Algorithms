c0536ea672a0bbe48e3d7f01aa818250f1d2c279
unknown <thakurramanji@gmail.com>
2023-05-04 16:48:43 +0530

    pepe

---
+++ 8.Char_array_and_2d_array/trim_spaces.cpp
@@ -0,0 +1,21 @@
+
+void trimSpaces(char input[]) {
+    // Write your code here
+    
+    int n=0;
+    for(int i=0;input[i]!='\0';i++)
+        n++;
+     if(input[0]=='\0')
+    return;
+
+//      if(input[0]==' '){
+
+//     for(int i=0;i<n;i++){
+//         input[i]=input[i+1];
+//     }
+//     trimSpaces(input);
+//     }
+//     else{
+//     trimSpaces(input+1);
+//    }
+}
